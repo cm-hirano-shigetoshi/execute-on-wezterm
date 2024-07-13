78ef705278776e6a88951e94dc09b48efb0a11fd
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-07-14 00:35:37 +0900

    最低限の機能を実装

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+WEZTERM_APP_DIR="${WEZTERM_APP_DIR-/Applications/WezTerm.app/Contents/MacOS}"
+
+pane_id=$("${WEZTERM_APP_DIR}/wezterm" cli split-pane)
+"${WEZTERM_APP_DIR}/wezterm" cli zoom-pane --pane-id ${pane_id} 
+echo "exec bash -c '$@'" | "${WEZTERM_APP_DIR}/wezterm" cli send-text --pane-id ${pane_id} --no-paste
