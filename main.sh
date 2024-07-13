b34bfb7fc3bf522e17e9dd10830b22e3fd59ae07
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-07-14 01:54:34 +0900

    execを排除

--- main.sh
+++ main.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 WEZTERM_APP_DIR="${WEZTERM_APP_DIR-/Applications/WezTerm.app/Contents/MacOS}"
 
 pane_id=$("${WEZTERM_APP_DIR}/wezterm" cli split-pane)
-"${WEZTERM_APP_DIR}/wezterm" cli zoom-pane --pane-id ${pane_id} 
-echo "exec bash -c '$@'" | "${WEZTERM_APP_DIR}/wezterm" cli send-text --pane-id ${pane_id} --no-paste
+"${WEZTERM_APP_DIR}/wezterm" cli zoom-pane --pane-id ${pane_id}
+echo "$@; exit" | "${WEZTERM_APP_DIR}/wezterm" cli send-text --pane-id ${pane_id} --no-paste
